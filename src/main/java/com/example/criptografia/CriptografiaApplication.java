456cd93c1570ca8a551ab7d665fb282d442302f0
EduardTeixeira <eduardteixeira01@gmail.com>
2022-07-01 11:06:39 -0300

    first commit

---
+++ src/main/java/com/example/criptografia/CriptografiaApplication.java
@@ -0,0 +1,13 @@
+package com.example.criptografia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CriptografiaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CriptografiaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/criptografia/CriptografiaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.criptografia;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CriptografiaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
